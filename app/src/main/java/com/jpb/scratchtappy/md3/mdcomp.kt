e61302751bd83d0f0062dcfb6554409050288dc3
jpb <jakub.baranski.33@gmail.com>
2022-01-15 13:53:27 +0000

    v1.5 DP3 Git sync

--- app/src/main/java/com/jpb/scratchtappy/md3/mdcomp.kt
+++ app/src/main/java/com/jpb/scratchtappy/md3/mdcomp.kt
@@ -7,5 +7,8 @@ class mdcomp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mdcomp)
+        val ab: androidx.appcompat.app.ActionBar? = supportActionBar
+        ab?.setTitle("MD Components")
+        ab?.setSubtitle("ADL, Holo, MD, MD3")
     }
 }
