677b34ef8dd62f3820f60c86da3bab352616e599
jpb <jakub.baranski.33@gmail.com>
2022-01-23 18:42:39 +0000

    v2.0 APKInfo updates

--- app/src/main/java/com/jpb/scratchtappy/md3/APKInfo.kt
+++ app/src/main/java/com/jpb/scratchtappy/md3/APKInfo.kt
@@ -11,6 +11,6 @@ class APKInfo : AppCompatActivity() {
         setContentView(R.layout.activity_apkinfo2)
         val ab: androidx.appcompat.app.ActionBar? = supportActionBar
         ab?.setTitle("APK Info")
-        ab?.setSubtitle("ScratchTappy md3 1.5")
+        ab?.setSubtitle("ScratchTappy md3 2.0")
     }
 }
