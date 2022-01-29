60cd563415403a7d436305dde05c39e7f212ef2f
jpb <jakub.baranski.33@gmail.com>
2022-01-29 17:40:39 +0000

    v2.0 Enhanced Introduces new, revolutionary APKInfo changes!

--- app/src/main/java/com/jpb/scratchtappy/md3/APKInfo.kt
+++ app/src/main/java/com/jpb/scratchtappy/md3/APKInfo.kt
@@ -1,8 +1,13 @@
 package com.jpb.scratchtappy.md3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
 import com.google.android.material.color.DynamicColors
+
 
 class APKInfo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +17,24 @@ class APKInfo : AppCompatActivity() {
         val ab: androidx.appcompat.app.ActionBar? = supportActionBar
         ab?.setTitle("APK Info")
         ab?.setSubtitle("ScratchTappy md3 2.0 Enhanced")
+        val chart = findViewById<PieChart>(R.id.chart)
+        val entries: MutableList<PieEntry> = ArrayList()
+        entries.add(PieEntry(55.7f, "classes.dex"))
+        entries.add(PieEntry(14.7f, "res"))
+        entries.add(PieEntry(8.2f, "classes11.dex"))
+        entries.add(PieEntry(7.2f, "assets"))
+        entries.add(PieEntry(5.1f, "lib"))
+        entries.add(PieEntry(3.6f, "resources.arsc"))
+        entries.add(PieEntry(2.8f, "classes2.dex"))
+        entries.add(PieEntry(1.3f, "META-INF"))
+        entries.add(PieEntry(0.6f, "classes10.dex"))
+        entries.add(PieEntry(0.2f, "classes3.dex"))
+        entries.add(PieEntry(0.1f, "kotlin"))
+        entries.add(PieEntry(0.1f, "classes4.dex"))
+        val set = PieDataSet(entries, "Composition of thge ST md3 " + BuildConfig.VERSION_NAME + " app/apk")
+        val data = PieData(set)
+        chart.setData(data)
+        chart.invalidate() // refresh
+
     }
 }
