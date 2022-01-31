1ae65f3f2c6b6600cf1bee1fc709fca2ee395a7c
jpb <jakub.baranski.33@gmail.com>
2022-01-31 19:29:39 +0000

    v2.0 Enhanced RC2 Fix: Small changes in pie chart Fix: Text 'Line Chart' now not scrollable Fix: Scroller gap in About screen fixed Fix: Updates APKInfo with latest info

--- app/src/main/java/com/jpb/scratchtappy/md3/APKInfo.kt
+++ app/src/main/java/com/jpb/scratchtappy/md3/APKInfo.kt
@@ -9,6 +9,7 @@ import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.google.android.material.color.DynamicColors
 
+
 class APKInfo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +20,11 @@ class APKInfo : AppCompatActivity() {
         ab?.setSubtitle("ScratchTappy md3 2.0 Enhanced")
         val chart = findViewById<PieChart>(R.id.chart)
         val entries: MutableList<PieEntry> = ArrayList()
-        entries.add(PieEntry(55.7f, "classes.dex"))
-        entries.add(PieEntry(14.7f, "res"))
-        entries.add(PieEntry(8.2f, "classes11.dex"))
-        entries.add(PieEntry(7.2f, "assets"))
-        entries.add(PieEntry(5.1f, "lib"))
+        entries.add(PieEntry(54.5f, "classes.dex"))
+        entries.add(PieEntry(14.4f, "res"))
+        entries.add(PieEntry(10.1f, "classes11.dex"))
+        entries.add(PieEntry(7.1f, "assets"))
+        entries.add(PieEntry(5.0f, "lib"))
         entries.add(PieEntry(3.6f, "resources.arsc"))
         entries.add(PieEntry(2.8f, "classes2.dex"))
         entries.add(PieEntry(1.3f, "META-INF"))
@@ -31,11 +32,14 @@ class APKInfo : AppCompatActivity() {
         entries.add(PieEntry(0.2f, "classes3.dex"))
         entries.add(PieEntry(0.1f, "kotlin"))
         entries.add(PieEntry(0.1f, "classes4.dex"))
+        entries.add(PieEntry(0.1f, "classes9.dex"))
         val colors: ArrayList<Int> = ArrayList()
         for (c in ColorTemplate.MATERIAL_COLORS) colors.add(c)
         val set = PieDataSet(entries, "Composition of the ST md3 " + BuildConfig.VERSION_NAME + " app/apk")
         set.setColors(colors);
         val data = PieData(set)
+        val legend = chart.legend
+        legend.isEnabled = false
         chart.setData(data)
         chart.invalidate() // refresh
     }
